c783cecd962e830a24981b006a4004d30a8c6417
chaosannals <chaosannals@outlook.com>
2024-02-17 17:23:14 +0800

    save

--- baredemo/src/App.js
+++ baredemo/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Chess3x3 from './pages/Chess3x3';
+
 function App() {
   return (
     <div className="App">
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Chess3x3 />
     </div>
   );
 }
