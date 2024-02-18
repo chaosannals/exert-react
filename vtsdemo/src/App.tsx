81b7c5901f728640a233cba06dcc37f0b38e3298
chaosannals <chaosannals@outlook.com>
2024-02-18 14:03:29 +0800

    save

--- vtsdemo/src/App.tsx
+++ vtsdemo/src/App.tsx
@@ -1,19 +1,20 @@
-import { useRoutes } from 'react-router-dom';
-import IndexPage from './pages/IndexPage';
+import { useRoutes } from "react-router-dom";
+import IndexPage from "./pages/IndexPage";
+import { lazy } from "react";
 
 function App() {
   const routes = useRoutes([
     {
-      path: '/',
-      element: <IndexPage/>,
+      path: "/",
+      element: <IndexPage />,
+    },
+    {
+      path: "*",
+      Component: lazy(() => import("./pages/ErrorPage")),
     },
   ]);
 
-  return (
-    <>
-      {routes}
-    </>
-  )
+  return <>{routes}</>;
 }
 
-export default App
+export default App;
---
+++ vtsdemo/src/pages/ErrorPage.tsx
@@ -0,0 +1,8 @@
+import styles from './ErrorPage.module.scss'
+
+export default function ErrorPage() {
+    return (
+        <div className={styles.page}>
+        </div>
+    )
+}
