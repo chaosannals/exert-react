12d2383a0af61eb7d04a884f5695c3827a2e04fc
chaosannals <chaosannals@outlook.com>
2024-02-18 11:09:01 +0800

    save

--- baretsdemo/src/App.tsx
+++ baretsdemo/src/App.tsx
@@ -2,10 +2,11 @@
 // 此处由于引入的其他组件已经引入过了，所以可以不用。
 // 如果没有关联的组件不引入 就会报错。
 // import React from 'react';
+import { lazy } from "react";
 import { useRoutes } from "react-router-dom";
 
 import IndexPage from "./pages/IndexPage";
-import ErrorPage from "./pages/ErrorPage";
+// import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   // useRoutes 居然必须再函数体内调用。
@@ -16,7 +17,7 @@ function App() {
     },
     {
       path: "*",
-      element: <ErrorPage />,
+      Component: lazy(() => import('./pages/ErrorPage')),
     },
   ]);
   return <div className="App">{routes}</div>;
