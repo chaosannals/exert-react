f2955ccca55dcfd810c2dbb579f3e965efafbeb4
chenshenchao <chenshenchao@outlook.com>
2024-05-22 01:41:19 +0800

    save

--- vtsdemo/src/pages/msic/StorePage.tsx
+++ vtsdemo/src/pages/msic/StorePage.tsx
@@ -1,32 +1,51 @@
-import { useStore } from "react-redux";
 import { incremented, decremented } from "../../stores/counter";
-import { store } from "../../stores";
+import { rememberFullname, store } from "../../stores";
 import { useState } from "react";
+import { setFullname } from "../../stores/some";
 
 export default function StorePage() {
-  const [counter, setCounter] = useState(store.getState().counter.value);
+  const state = store.getState();
+  const [counter, setCounter] = useState(state.counter.value);
+  const [firstName, lastName, setFirstName, setLastName] = rememberFullname();
 
   store.subscribe(() => {
-    setCounter(store.getState().counter.value);
+    const state = store.getState();
+    setCounter(state.counter.value);
   });
 
   return (
     <div>
-      <button
-        onClick={() => {
-          store.dispatch(incremented());
-        }}
-      >
-        +
-      </button>
-      <span>{counter}</span>
-      <button
-        onClick={() => {
-          store.dispatch(decremented());
-        }}
-      >
-        -
-      </button>
+      <div>
+        <button
+          onClick={() => {
+            store.dispatch(incremented());
+          }}
+        >
+          +
+        </button>
+        <span>{counter}</span>
+        <button
+          onClick={() => {
+            store.dispatch(decremented());
+          }}
+        >
+          -
+        </button>
+      </div>
+      <div>
+        <input
+          value={firstName}
+          onChange={(e) => {
+            setFirstName(e.target.value);
+          }}
+        />
+        <input
+          value={lastName}
+          onChange={(e) => {
+            setLastName(e.target.value);
+          }}
+        />
+      </div>
     </div>
   );
 }
--- vtsdemo/src/stores/index.ts
+++ vtsdemo/src/stores/index.ts
@@ -1,14 +1,36 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { counterSlice } from './counter'
 import { timerSlice } from './timer'
+import { someSlice, setFullname } from './some'
+import { useState } from 'react'
 
 export const store = configureStore({
     reducer: {
         counter: counterSlice.reducer,
         timer: timerSlice.reducer,
+        some: someSlice.reducer,
     }
 })
 
 store.subscribe(() => {
     console.log(store.getState())
 })
+
+export const rememberFullname = (): [string, string, (first: string) => void, (last: string) => void] => {
+    const state = store.getState();
+    const [firstName, setFirstName] = useState(state.some.firstName);
+    const [lastName, setLastName] = useState(state.some.lastName);
+
+    store.subscribe(() => {
+        const state = store.getState();
+        setFirstName(state.some.firstName);
+        setLastName(state.some.lastName);
+    });
+    return [firstName, lastName, (first: string) => {
+        const state = store.getState();
+        store.dispatch(setFullname({ first, last: state.some.lastName }))
+    }, (last: string) => {
+        const state = store.getState();
+        store.dispatch(setFullname({ first: state.some.firstName, last }))
+    }];
+}
